6f3a90456a636c188f69f26ce79357ddaa72fc93
Adam Tomeček <adam.tomecek@gmail.com>
2022-02-18 16:51:19 +0100

    setup

---
+++ config/initializers/generators.rb
@@ -0,0 +1,8 @@
+Rails.application.config.generators do |g|
+  g.assets            false
+  g.helper            false
+  g.request_specs     false
+  g.view_specs        false
+  g.controller_specs  false
+  g.routing_specs     false
+end
