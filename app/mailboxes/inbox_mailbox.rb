23231ce257a3968bf84a0223a1dec6e052f41b21
Adam Tomeček <adam.tomecek@gmail.com>
2024-04-13 11:03:24 +0200

    fourth part

---
+++ app/mailboxes/inbox_mailbox.rb
@@ -0,0 +1,45 @@
+class InboxMailbox < ApplicationMailbox
+  attr_reader :message
+
+  def process
+    @message = Message.create!(
+      from: mail.from.join(', '),
+      to: mail.to.join(', '),
+      subject: mail.subject,
+      attachments: parsed_attachments.map { |attachment| attachment[:blob] },
+      content: process_email_content,
+    )
+  end
+
+  private
+
+  def process_email_content
+    if mail.html_part.present?
+      document = Nokogiri::HTML(mail.html_part.body.decoded)
+      document.at_css("body").inner_html.encode('utf-8')
+    elsif mail.text_part.present?
+      mail.text_part.body.decoded
+    else
+      mail.body.decoded
+    end
+  end
+
+  def parsed_attachments
+    @parsed_attachments ||= mail.attachments.map do |attachment|
+      blob = blob_from_attachment(attachment)
+
+      {
+        attachment: attachment,
+        blob: blob
+      }
+    end
+  end
+
+  def blob_from_attachment(attachment)
+    ActiveStorage::Blob.create_and_upload!(
+      io: StringIO.new(attachment.decoded),
+      filename: attachment.filename,
+      content_type: attachment.content_type,
+    )
+  end
+end
---
+++ spec/mailboxes/inbox_mailbox_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe InboxMailbox, type: :mailbox do
+  it 'routes emails to correct mailbox' do
+    expect(InboxMailbox).to receive_inbound_email(to: 'test@example.com')
+  end
+
+  it 'marks e-mail as delivered and creates new Message' do
+    mail = Mail.new(
+      from: 'from@example.com',
+      to: 'test@example.com',
+      subject: 'Test email',
+      body: 'Test body',
+    )
+
+    mail_processed = process(mail)
+
+    expect(mail_processed).to have_been_delivered
+    expect(Message.count).to eq 1
+
+    message = Message.last
+    expect(message.from).to eq 'from@example.com'
+    expect(message.to).to eq 'test@example.com'
+    expect(message.subject).to eq 'Test email'
+  end
+
+  it 'saves attachments' do
+    mail = Mail.new(
+      from: 'from@example.com',
+      to: 'test@example.com',
+      subject: 'Test email',
+      body: 'Test body',
+    )
+
+    mail.attachments['test.csv'] = { mime_type: 'text/csv', content: 'a,b,c' }
+    mail.attachments['test.png'] = { mime_type: 'image/png', content: 'Not PNG' }
+
+    mail_processed = process(mail)
+
+    message = Message.last
+
+    expect(message.attachments.count).to eq 2
+    expect(message.attachments[0].blob.filename).to eq 'test.csv'
+    expect(message.attachments[1].blob.filename).to eq 'test.png'
+  end
+
+  it 'uses HTML body' do
+    mail = Mail.new(
+      from: 'from@example.com',
+      to: 'test@example.com',
+      subject: 'Test email',
+    )
+
+    text_part = Mail::Part.new do
+      body 'This is plain text'
+    end
+
+    html_part = Mail::Part.new do
+      content_type 'text/html; charset=UTF-8'
+      body '<h1>This is HTML</h1>'
+    end
+
+    mail.text_part = text_part
+    mail.html_part = html_part
+
+    mail_processed = process(mail)
+    message = Message.last
+
+    expect(message.content.to_plain_text).to eq 'This is HTML'
+  end
+end
+
+
